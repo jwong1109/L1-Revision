39be408b4c53a2d95df13e75595c49a03b4a04ba
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-02-07 13:25:19 +1300

    Update 02_sum_numbers_range.py

--- 02_sum_numbers_range.py
+++ 02_sum_numbers_range.py
@@ -1,6 +1,10 @@
-numbers = [20, 21, 22, 33, 24, 25]
-for item in numbers:
-    print(item)
-print("-----")
-print(sum(numbers))
+""" Sum of numbers in a range v1
+Created by Joseph Wong
+5/02/2022
+"""
 
+numbers = [20, 21, 22, 33, 24, 25]  # Lists numbers from 20 to 25 inclusive
+for item in numbers:
+    print(item)  # prints each number in the list
+print("-----")  # Adds a line between the numbers and the sum
+print(sum(numbers))  # Sum of the numbers
