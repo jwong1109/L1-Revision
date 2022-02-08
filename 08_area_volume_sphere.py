4af31e6536db3f7187b564e37083ea968e512b01
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-02-08 15:54:33 +1300

    Update 08_area_volume_sphere.py

--- 08_area_volume_sphere.py
+++ 08_area_volume_sphere.py
@@ -7,7 +7,7 @@ import math
 # Input radius of the sphere
 radius = float(input("Enter the radius of the sphere: "))
 # Calculate volume based on the formula, rounded to 2 decimal places
-volume = round (4/3 * math.pi * radius * radius * radius, 2)
+volume = round(4 / 3 * math.pi * radius * radius * radius, 2)
 # Calculate surface area based on the formula, rounded to 2 decimal places
 surface_area = round(4 * math.pi * radius * radius, 2)
 
